51cd892250c6a0451ce73eb34ee39cc88c898bb8
touru <torufurukawa+touru@gmail.com>
2024-04-28 08:35:22 +0900

    Add inter-process communication

--- preload.js
+++ preload.js
@@ -1,7 +1,9 @@
+const { ipcRenderer } = require('electron');
 const { contextBridge } = require('electron/renderer');
 
 contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke('ping')
 })
--- renderer.js
+++ renderer.js
@@ -1,2 +1,9 @@
 const information = document.getElementById('info');
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
+
+const callPing = async () => {
+  const response = await window.versions.ping();
+  const debug = document.getElementById('debug');
+  debug.innerText = response;
+}
+callPing();
